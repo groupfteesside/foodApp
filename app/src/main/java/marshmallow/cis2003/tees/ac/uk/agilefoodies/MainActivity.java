225b0b09f161ee41f73111db130ba38c277ecbc5
Ashley James Dacre RENWICK <s6118209@linux41.scm.tees.ac.uk>
2019-02-11 10:53:47 +0000

    Initial commit

---
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/MainActivity.java
@@ -0,0 +1,86 @@
+package marshmallow.cis2003.tees.ac.uk.agilefoodies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Juli
+
+
+
+
+
+
+
+        // Matthew -->
+
+
+
+
+
+
+        // Jack -->
+
+
+
+
+
+
+        // Marshall -->
+
+
+
+
+
+        // Ash -->
+
+
+
+
+
+
+
+    }
+
+    // Juli -->
+
+
+
+
+
+
+
+        // Matthew -->
+
+
+
+
+
+
+        // Jack -->
+
+
+
+
+
+
+        // Marshall -->
+
+
+
+
+
+        // Ash -->
+
+
+
+
+
+
+
+}
