329ae7d156ca6ccd1d0337f98a525ce40e51ab23
Marshall Brandon FESSEY <s6115598@linux33.scm.tees.ac.uk>
2019-02-12 14:59:39 +0000

    Added basic Food Group class

--- SR28parser/src/objects/FoodGroup.java
+++ SR28parser/src/objects/FoodGroup.java
@@ -5,10 +5,30 @@
  */
 package objects;
 
+import static objects.SR28Helper.trimField;
+
 /**
  *
  * @author s6115598
  */
-public class FoodGroup {
+public class FoodGroup 
+{
+    private int id;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
     
+    // Construct from line of FD_GROUP.txt
+    public FoodGroup(String inLine)
+    {
+        String[] splitLine = inLine.split("\\^");
+        id = Integer.parseInt(trimField(splitLine[0]));
+        description = trimField(splitLine[1]);
+    }
 }
